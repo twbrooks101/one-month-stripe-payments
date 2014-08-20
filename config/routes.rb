dfdcca6eba7bbb30bf06cf19a233dac8f5cc24db
Tyler Brooks <tyler@Tylers-MacBook-Pro.local>
2014-08-20 09:53:39 -0600

    Created simple hello world home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  # This means within pages controller it will run home method.
+  root 'pages#home'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
